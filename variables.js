36663a5136e779a207c964d27f9f37a5205902cf
MuditThakur <muditthakur10@gmail.com>
2024-05-28 23:10:39 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,14 @@
+const accountId =123
+let accEmail = "mudit12"
+var accPass ="0000"
+accCity ="Dalhousie"
+
+// accountId =2
+accEmail ="ww"
+accPass ="999"
+accCity="kk"
+console.log(accountId);
+console.log(accEmail);
+console.log(accountId);
+
+console.table([accEmail,accPass,accountId,accCity])
