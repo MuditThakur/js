74985f81d581856b917a5cc91c7a43e9918a26a0
MuditThakur <muditthakur10@gmail.com>
2024-06-27 22:51:00 +0530

    strings-basic

---
+++ Js-prep/Strings/string.js
@@ -0,0 +1,12 @@
+let name  = "mudit"
+let caste = "thakur"
+
+console.log(caste[0]);
+
+//string template and string interpolation
+let msg = `my name is ${name} and my caste is ${caste}`
+console.log(msg)
+
+//escape sequence character
+let fruit = 'bana\'na'
+console.log(fruit)
