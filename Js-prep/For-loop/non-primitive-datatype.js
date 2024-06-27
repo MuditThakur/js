d88c026fbb6e3f4eb78989d43ddffa29208c4363
MuditThakur <muditthakur10@gmail.com>
2024-06-27 11:24:42 +0530

    updates

---
+++ Js-prep/For-loop/non-primitive-datatype.js
@@ -0,0 +1,18 @@
+const item = {
+  mudit: "thakur",
+  salary: 50000,
+  age: 20,
+  isMale: true,
+};
+console.log(item["mudit"]);
+console.log(typeof (6 + 7));
+console.log(typeof item);
+let item2 = {
+  mudit: "thakur",
+  salary: 50000,
+  age: 20,
+  isMale: true,
+};
+item2["mudit"] = "Kumar";
+console.log(item2["mudit"]);
+console.log(item2["mudit"]);
